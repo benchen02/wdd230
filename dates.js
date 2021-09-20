f56009e733b6c93fe20ae94916722b8ff58d4e84
benchen02 <che16003@byui.edu>
2021-09-20 10:28:52 -0600

    Remove duplicate files

--- dates.js
+++
@@ -1,6 +0,0 @@
-const copyrightyear = document.getElementById("copyrightyear");
-copyrightyear.textContent = new Date().getFullYear();
-
-
-const datemodified = document.getElementById("datemodified");
-datemodified.textContent = new Date (document.lastModified).toLocaleString();
