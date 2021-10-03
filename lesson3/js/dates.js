e87673b101b03c111a01b5e9999c9e222abf3c4e
benchen02 <che16003@byui.edu>
2021-10-02 22:45:24 -0600

    Week 3 fix problems tomorrow

---
+++ lesson3/js/dates.js
@@ -0,0 +1,6 @@
+const copyrightyear = document.getElementById("copyrightyear");
+copyrightyear.textContent = new Date().getFullYear();
+
+
+const datemodified = document.getElementById("datemodified");
+datemodified.textContent = document.lastModified;
