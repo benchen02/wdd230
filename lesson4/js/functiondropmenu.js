5e63a2483d34990ad18cd036475c72eb016d13b6
benchen02 <che16003@byui.edu>
2021-10-09 22:45:36 -0600

    unfinished dropdown menu

---
+++ lesson4/js/functiondropmenu.js
@@ -0,0 +1,17 @@
+function functiondropmenu() {
+    var dropmenu = document.getElementById("menu");
+    if (dropmenu.style.display === "block") {
+      dropmenu.style.display = "none";
+    } else {
+      dropmenu.style.display = "block";
+    }
+  }
+
+function myFunction() {
+    var x = document.getElementById("hammenu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
