974c64ab62d616632d6e9a15de5b82bb4af16104
benchen02 <che16003@byui.edu>
2022-01-05 20:40:37 -0700

    New semester new files

--- lesson4/js/dates.js
+++
@@ -1,6 +0,0 @@
-const copyrightyear = document.getElementById("copyrightyear");
-copyrightyear.textContent = new Date().getFullYear();
-
-
-const datemodified = document.getElementById("datemodified");
-datemodified.textContent = document.lastModified;
--- lesson4/js/functiondropmenu.js
+++
@@ -1,8 +0,0 @@
-function functiondropmenu() {
-    var dropmenu = document.getElementById("menu");
-    if (dropmenu.style.display === "block") {
-      dropmenu.style.display = "none";
-    } else {
-      dropmenu.style.display = "block";
-    }
-  }
