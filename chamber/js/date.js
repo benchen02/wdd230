8595a78e592acfb3bdf4864b1f0ced314f20ffe6
benchen02 <che16003@byui.edu>
2022-10-08 09:15:52 -0600

    Chamber Home Page

---
+++ chamber/js/date.js
@@ -0,0 +1,10 @@
+const lastmodfulldate = document.querySelector('#currentdate');
+currentdate.textContent = document.lastModified;
+
+const currentyear = document.querySelector('#currentyear');
+currentyear.textContent = new Date().getFullYear();
+
+const datefield = document.querySelector("#date");
+const now = Date.now();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+datefield.innerHTML = fulldate;
