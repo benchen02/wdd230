794e607714764f92f0e5c1442f0e4af4167db1cc
benchen02 <che16003@byui.edu>
2022-10-22 19:38:48 -0600

    update

---
+++ chamber/js/windchill.js
@@ -0,0 +1,23 @@
+const tempFinput = document.querySelector('#tempF');
+const windspdinput = document.querySelector('#windspd');
+const windchill = document.querySelector('#windchl');
+
+
+tempFinput.addEventListener('change', calcwindspeed);
+windspdinput.addEventListener('change', calcwindspeed);
+
+function calcwindspeed(event){
+
+    const tempF = parseInt(tempFinput.value);
+    const windspd = parseInt(windspdinput.value);
+
+    if (tempF <= 50 && windspd > 3) {
+       result = Math.round((35.74+(0.6215*tempF)-(35.75*(windspd^0.16))+(0.4275 *tempF*(windspd^0.16)))*100)/100;
+    } else if (tempF >= 50 && windspd < 3) {
+        result = 'N/A';
+    } else {
+        result = '';
+    }
+    
+    windchill.textContent = result;
+}
