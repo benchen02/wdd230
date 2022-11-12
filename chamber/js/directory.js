2413aec3ce0e66de5cf4945899c142f698041cd6
benchen02 <che16003@byui.edu>
2022-11-11 20:13:31 -0700

    directory update

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -2,10 +2,44 @@ const requestURL =
   "https://raw.githubusercontent.com/benchen02/wdd230/master/chamber/json/data.json";
 const cards = document.querySelector(".grid");
 
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-  });
+async function getInfo (){
+    const response = await fetch (requestURL);
+    const data = await response.json();
+    data.business.forEach(business => { displayInfo(business)  });
+}
+
+function displayInfo(business){
+    //Create HTML Elements
+    let card = document.createElement("section");
+    let h4 = document.createElement("h4");
+    let logo = document.createElement("img");
+    let address = document.createElement("p");
+    let phone = document.createElement("p");
+    let site = document.createElement("a");
+
+    //Change respective textcontent to contain business name, full address, and phone number
+    h4.textContent = `${business.name}`;
+    address.textContent = `${business.street} ${business.city} ${business.state} ${business.zip}`;
+    phone.textContent = `${business.phone}`
+    site.innerHTML = business.siteURL;
+
+    //Building attrubutes for img and link
+    logo.setAttribute("src", business.logoURL);
+    logo.setAttribute(
+      "alt",
+      `${business.name} logo`
+    );
+    logo.setAttribute("loading", "lazy");
+    site.setAttribute("href", business.siteURL);
+
+    //Append section with information
+    card.appendChild(logo);
+    card.appendChild(h4);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(site);
+
+    cards.appendChild(card);
+}
+
+getInfo();
