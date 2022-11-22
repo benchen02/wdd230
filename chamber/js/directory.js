52140750412eb733f55f652fd0b71a69c3ef2ded
benchen02 <che16003@byui.edu>
2022-11-21 19:31:21 -0700

    directory update

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -16,12 +16,14 @@ function displayInfo(business){
     let address = document.createElement("p");
     let phone = document.createElement("p");
     let site = document.createElement("a");
+    let phrase = document.createElement("p")
 
     //Change respective textcontent to contain business name, full address, and phone number
     h4.textContent = `${business.name}`;
     address.textContent = `${business.street} ${business.city} ${business.state} ${business.zip}`;
     phone.textContent = `${business.phone}`
     site.innerHTML = business.siteURL;
+    phrase.innerHTML = business.phrase;
 
     //Building attrubutes for img and link
     logo.setAttribute("src", business.logoURL);
@@ -36,6 +38,7 @@ function displayInfo(business){
     //Append section with information
     card.appendChild(logo);
     card.appendChild(h4);
+    card.appendChild(phrase);
     card.appendChild(address);
     card.appendChild(phone);
     card.appendChild(site);
