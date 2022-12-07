6f1a02aae14ec47abbe9467ef552cded632f80ce
benchen02 <che16003@byui.edu>
2022-12-06 20:08:44 -0700

    Bountiful Foods Project

--- bountiful/js/windchill.js
+++ bountiful/js/windchill.js
@@ -1,4 +1,4 @@
-const url = "https://api.openweathermap.org/data/2.5/forecast?q=Carlsbad&units=imperial&cnt=9&APPID=6a11094bbff7a2a4374e1a946eca61c8"
+const url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.15&lon=117.35&exclude=hourly&units=imperial&APPID=6a11094bbff7a2a4374e1a946eca61c8"
 
 async function apiFetch() {
     try {
